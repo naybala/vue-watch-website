36aced5a70eed1c6af81219ab674a24399811f75
naybala <naybalaismylife.nbl@gmail.com>
2022-06-01 17:20:25 +0630

    adding show case side decorate

--- src/components/ShowCase.vue
+++ src/components/ShowCase.vue
@@ -1,5 +1,19 @@
 <template>
   <section class="p-5 p-lg-0 pt-lg-5 text-center text-sm-start" data-aos="fade-down">
+    <div class="leftShowCase-1">
+      <div class="container-left"></div>
+    </div>
+    <div class="leftShowCase-2">
+      <div class="container-left"></div>
+    </div>
+
+    <div class="rightShowCase-1">
+      <div class="container-right"></div>
+    </div>
+    <div class="rightShowCase-2">
+      <div class="container-right"></div>
+    </div>
+
     <div class="container">
       <div class="d-sm-flex align-items-center justify-content-between">
         <img
@@ -30,4 +44,52 @@
 
 <script></script>
 
-<style></style>
+<style scoped>
+.leftShowCase-1 {
+  position: absolute;
+  left: -5rem;
+  transform: rotate(-40deg);
+}
+
+.leftShowCase-2 {
+  position: absolute;
+  left: -5rem;
+  bottom: 1rem;
+  transform: rotate(-40deg);
+}
+
+.rightShowCase-1 {
+  position: absolute;
+  right: -5rem;
+  transform: rotate(40deg);
+}
+
+.rightShowCase-2 {
+  position: absolute;
+  right: -5rem;
+  bottom: 1rem;
+  transform: rotate(40deg);
+}
+.container-right {
+  width: 350px;
+  height: 100px;
+  background-color: #6c63ff;
+  border-top-left-radius: 5rem;
+  border-bottom-left-radius: 5rem;
+}
+
+.container-left {
+  width: 350px;
+  height: 100px;
+  background-color: #6c63ff;
+  border-top-right-radius: 5rem;
+  border-bottom-right-radius: 5rem;
+}
+
+@media (max-width: 1100px) {
+  .container-left,
+  .container-right {
+    display: none;
+  }
+}
+</style>
